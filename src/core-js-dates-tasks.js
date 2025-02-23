2da503562def50169db6cc4d1701678c4ada562e
cayman444 <artursavinX@yandex.ru>
2025-02-23 19:54:40 +0300

    feat: add solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -100,8 +100,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -115,8 +115,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const millisecondsInDay = 86400000;
+  const diff = new Date(dateEnd) - new Date(dateStart);
+
+  return Math.floor(diff / millisecondsInDay) + 1;
 }
 
 /**
@@ -136,8 +139,18 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const { start, end } = period;
+
+  const startInterval = Date.parse(start);
+  const endInterval = Date.parse(end);
+  const currentInterval = Date.parse(date);
+
+  if (currentInterval >= startInterval && currentInterval <= endInterval) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
@@ -151,8 +164,11 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en', {
+    timeZone: 'UTC',
+    hour12: true,
+  });
 }
 
 /**
@@ -167,8 +183,21 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const currentMonth = month - 1;
+  const date = new Date(year, currentMonth);
+  let countWeekend = 0;
+
+  while (date.getMonth() === currentMonth) {
+    const day = date.getDay();
+    if (day === 0 || day === 6) {
+      countWeekend += 1;
+    }
+
+    date.setDate(date.getDate() + 1);
+  }
+
+  return countWeekend;
 }
 
 /**
